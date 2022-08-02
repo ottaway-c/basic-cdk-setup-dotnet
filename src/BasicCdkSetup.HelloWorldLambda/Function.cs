ada434d314d3b5be80ad1f2c36593cadec252d42
Jake Scott <jake.net@gmail.com>
2022-08-03 06:14:45 +1200

    fmt

--- src/BasicCdkSetup.HelloWorldLambda/Function.cs
+++ src/BasicCdkSetup.HelloWorldLambda/Function.cs
@@ -4,33 +4,33 @@ using AWS.Lambda.Powertools.Logging;
 
 [assembly: LambdaSerializer(typeof(DefaultLambdaJsonSerializer))]
 
-namespace BasicCdkSetup.HelloWorldLambda
-{
-    public class HelloWorldRequest
-    {
-        public string? Name { get; set; }
-    }
-    
-    public class HelloWorldResponse
-    {
-        public HelloWorldResponse(string message)
-        {
-            Message = message;
-        }
+namespace BasicCdkSetup.HelloWorldLambda;
 
-        public string Message { get; }
-    }
-    
-    public class Function
+public class HelloWorldRequest
+{
+    public string? Name { get; set; }
+}
+
+public class HelloWorldResponse
+{
+    public HelloWorldResponse(string message)
     {
-        [Logging(LogEvent = true, ClearState = true)]
-        public HelloWorldResponse FunctionHandler(HelloWorldRequest request, ILambdaContext context)
-        {
-            Logger.LogInformation(new { Name = request.Name, Nested = new { SomeArg = 1, AnotherArg = 2 }  }, "This is a info level message");
-            
-            var message = request.Name == null ? "Hello World" : $"Hello {request.Name}";
-            
-            return new HelloWorldResponse(message);
-        }
+        Message = message;
+    }
+
+    public string Message { get; }
+}
+
+public class Function
+{
+    [Logging(LogEvent = true, ClearState = true)]
+    public HelloWorldResponse FunctionHandler(HelloWorldRequest request, ILambdaContext context)
+    {
+        Logger.LogInformation(new { Name = request.Name, Nested = new { SomeArg = 1, AnotherArg = 2 } },
+            "This is a info level message");
+
+        var message = request.Name == null ? "Hello World" : $"Hello {request.Name}";
+
+        return new HelloWorldResponse(message);
     }
 }
